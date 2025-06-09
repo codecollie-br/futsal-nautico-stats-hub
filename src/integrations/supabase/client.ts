6c18a53c178eca067ee34f2c6d5b4832128e54a8
codecollie-br <contato@codecollie.com.br>
2025-06-08 23:22:22 -0300

    .env

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://hsawlrjmsuduoyfypngj.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzYXdscmptc3VkdW95ZnlwbmdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzNDcyMzgsImV4cCI6MjA2NDkyMzIzOH0.kkgksfU-LfQyfI-CEyU5N-uCA-HM1i3f7qT0av7XhF8";
+const SUPABASE_URL = process.env.SUPABASE_URL!;
+const SUPABASE_PUBLISHABLE_KEY = process.env.SUPABASE_KEY!;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
